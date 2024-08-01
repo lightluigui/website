2398d5716d01cab4fc2eb75b8096878e6908471e
Matharaujo <matheusaraujopoe@gmail.com>
2024-08-01 12:11:13 -0300

    first commit

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP curso em video</title>
+</head>
+<body>
+   <h1>
+   <?php
+      echo "ola, mundo! \u{1F30E}";
+   ?>
+   </h1>
+   <p>se livre da maldição</p>
+</body>
+</html>
+
+
